7483508e3db6ce095efef74432fa7000c0cecc67
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2018-10-25 22:20:02 +0200

    version 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='emojiflag',
-	version='1.0.1',
+	version='1.0.2',
 	url='https://github.com/lotrekagency/emojiflag',
 	license='MIT',
 	author='Lotrek',
